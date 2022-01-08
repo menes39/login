7a80d52a011bebd81a737b8d0a323e38360e9cb2
menes39 <menes39@hotmail.com>
2022-01-09 00:12:51 +0300

    commit

--- login.js
+++ login.js
@@ -1,4 +1,4 @@
-// Your web app's Firebase configuration
+// Firebase den alınan veriler
 var firebaseConfig = {
     apiKey: "AIzaSyAunBt08giONjPHVD164JqE9vzcArj4BDg",
   authDomain: "web-lo.firebaseapp.com",
@@ -9,15 +9,13 @@ var firebaseConfig = {
   appId: "1:87287137505:web:dba2483f7356c18796a3a0",
   measurementId: "G-S141HC9MRT"
   };
-  // Initialize Firebase
+ 
   firebase.initializeApp(firebaseConfig);
-  // Initialize variables
   const auth = firebase.auth()
   const database = firebase.database()
   
-  // Set up our register function
   function register () {
-    // Get all our input fields
+   
     email = document.getElementById('email').value
     password = document.getElementById('password').value
     full_name = document.getElementById('full_name').value
@@ -25,81 +23,72 @@ var firebaseConfig = {
 
     // Validate input fields
     if (validate_email(email) == false || validate_password(password) == false)  {
-      alert('Email or Password is Outta Line!!')
+      alert('Email ya da Password HATALI!')
       return
-      // Don't continue running the code
     }
     if (validate_field(full_name) == false){
-      alert('One or More Extra Fields is Outta Line!!')
+      alert('Hata!')
       return
     }
    
-    // Move on with Auth
     auth.createUserWithEmailAndPassword(email, password)
     .then(function() {
-      // Declare user variable
+
       var user = auth.currentUser
   
-      // Add this user to Firebase Database
+      // Realtime database e ekleme yap
       var database_ref = database.ref()
   
-      // Create User data
+      // data oluşturma
       var user_data = {
         email : email,
         full_name : full_name,
         last_login : Date.now()
       }
   
-      // Push to Firebase Database
+      // Firebase e atma
       database_ref.child('users/' + user.uid).set(user_data)
   
-      // DOne
-      alert('User Created!!')
+
+      alert('Başarılı! Kullanıcı Oluşturuldu')
     })
     .catch(function(error) {
-      // Firebase will use this to alert of its errors
+
       var error_code = error.code
       var error_message = error.message
   
       alert(error_message)
     })
   }
-  
-  // Set up our login function
+
   function login () {
-    // Get all our input fields
+
     email = document.getElementById('email').value
     password = document.getElementById('password').value
   
-    // Validate input fields
+
     if (validate_email(email) == false || validate_password(password) == false) {
-      alert('Email or Password is Outta Line!!')
+      alert('Email ya da Password HATALI!')
       return
-      // Don't continue running the code
+
     }
   
     auth.signInWithEmailAndPassword(email, password)
     .then(function() {
-      // Declare user variable
       var user = auth.currentUser
   
-      // Add this user to Firebase Database
       var database_ref = database.ref()
   
-      // Create User data
       var user_data = {
         last_login : Date.now()
       }
   
-      // Push to Firebase Database
       database_ref.child('users/' + user.uid).update(user_data)
   
-      // DOne
-      alert('User Logged In!!')
+      alert('Hoşgeldiniz !')
   
     })
     .catch(function(error) {
-      // Firebase will use this to alert of its errors
       var error_code = error.code
       var error_message = error.message
   
@@ -110,20 +99,17 @@ var firebaseConfig = {
   
   
   
-  // Validate Functions
   function validate_email(email) {
     expression = /^[^@]+@\w+(\.\w+)+\w$/
     if (expression.test(email) == true) {
-      // Email is good
       return true
     } else {
-      // Email is not good
       return false
     }
   }
   
   function validate_password(password) {
-    // Firebase only accepts lengths greater than 6
+    // 6 karakterden fazla girdi
     if (password < 6) {
       return false
     } else {
